2e8953d7d7e8e27f74c3591aec7a65dcbe62aff0
altonhudson <ah_1014@hotmail.com>
2025-08-13 20:42:21 -0400

    Added logic for Dark Mode Feature

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let details = document.querySelectorAll('.hide-project-details')
 for (let i = 0; i < detailsBtn.length; i++) {
     detailsBtn[i].addEventListener("click", function () {
         console.log(detailsBtn[i]);
-
+        
         for (let p = 0; p < details.length; p++) {
             if (detailsBtn[i].id == details[p].id) {
                 details[p].classList.toggle('show-project-details')
@@ -14,6 +14,10 @@ for (let i = 0; i < detailsBtn.length; i++) {
     })
 }
 
+//DARK MODE
+// let body = document.querySelector("body")
+// body.classList.toggle("dark-mode")
+
 let captureErrors = []
 
 const validate = () => {
@@ -21,19 +25,18 @@ const validate = () => {
 
     let name = document.getElementById("userName").value;
     if (name == "") {
-        captureErrors.push("name")
+        captureErrors.push("Name")
         isValid = false
     } 
-    
     let email = document.getElementById("email").value;
     if (email == "" || email.includes("@") == false) {
-        captureErrors.push("email")
+        captureErrors.push("Email")
         isValid = false
     } 
     
     let message = document.getElementById("message").value;
     if (message == "") {
-        captureErrors.push("message")
+        captureErrors.push("Message")
         isValid = false
     }else if (message.length < 36) {
         captureErrors.push("Message must be over 36 characters")
@@ -58,4 +61,12 @@ submit.addEventListener("click", (e) => {
         alert(`Please validate the following fields: ${captureErrors.join(", ")}.`)
         console.log(captureErrors);
     }
+});
+
+
+const darkModeBtn = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
 });
